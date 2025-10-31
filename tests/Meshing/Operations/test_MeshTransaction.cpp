3905e6bf26ca6b0348f019c6747ad3f09e83713c
Carl Sandström <sandstrom.carl@gmail.com>
2025-10-31 19:50:49 +0100

    Fix tests

--- tests/Meshing/Operations/test_MeshTransaction.cpp
+++ tests/Meshing/Operations/test_MeshTransaction.cpp
@@ -72,19 +72,19 @@ TEST_F(MeshTransactionTest, CommitTransactionTest)
     MeshTransaction transaction(meshData_.get());
     transaction.begin();
 
-    // Add an element during transaction
     auto element = std::make_unique<MockTetrahedralElement>(
         std::array<size_t, 4>{node1Id_, node2Id_, node3Id_, node4Id_});
+
+    EXPECT_EQ(meshData_->getElementCount(), 0);
     size_t elementId = meshData_->addElement(std::move(element));
 
+    EXPECT_EQ(meshData_->getElementCount(), 1);
     EXPECT_TRUE(transaction.isActive());
 
     transaction.commit();
 
+    EXPECT_EQ(meshData_->getElementCount(), 1);
     EXPECT_FALSE(transaction.isActive());
-
-    // Element should still exist after commit
-    // Note: Would need additional MeshData methods to verify element existence
 }
 
 TEST_F(MeshTransactionTest, RollbackTransactionTest)
@@ -92,23 +92,25 @@ TEST_F(MeshTransactionTest, RollbackTransactionTest)
     MeshTransaction transaction(meshData_.get());
     transaction.begin();
 
-    // Add an element during transaction
     auto element = std::make_unique<MockTetrahedralElement>(
         std::array<size_t, 4>{node1Id_, node2Id_, node3Id_, node4Id_});
+
+    EXPECT_EQ(meshData_->getElementCount(), 0);
     size_t elementId = meshData_->addElement(std::move(element));
 
     EXPECT_TRUE(transaction.isActive());
+    EXPECT_EQ(meshData_->getElementCount(), 1);
 
     transaction.rollback();
 
     EXPECT_FALSE(transaction.isActive());
-
-    // Element should be removed after rollback
-    // Note: Would need additional MeshData methods to verify element removal
+    EXPECT_EQ(meshData_->getElementCount(), 0);
 }
 
 TEST_F(MeshTransactionTest, AutoRollbackOnDestructorTest)
 {
+    EXPECT_EQ(meshData_->getElementCount(), 0);
+
     {
         MeshTransaction transaction(meshData_.get());
         transaction.begin();
@@ -118,49 +120,61 @@ TEST_F(MeshTransactionTest, AutoRollbackOnDestructorTest)
             std::array<size_t, 4>{node1Id_, node2Id_, node3Id_, node4Id_});
         size_t elementId = meshData_->addElement(std::move(element));
 
+        EXPECT_EQ(meshData_->getElementCount(), 1);
         EXPECT_TRUE(transaction.isActive());
 
         // Transaction should auto-rollback when going out of scope
     }
 
     // Element should be removed after auto-rollback
-    // Note: Would need additional MeshData methods to verify element removal
+    EXPECT_EQ(meshData_->getElementCount(), 0);
 }
 
 TEST_F(MeshTransactionTest, NodeAdditionAndRollbackTest)
 {
+    size_t initialNodeCount = meshData_->getNodeCount();
+
     MeshTransaction transaction(meshData_.get());
     transaction.begin();
 
     // Add a node during transaction
     size_t newNodeId = meshData_->addNode({2.0, 2.0, 2.0});
 
+    EXPECT_EQ(meshData_->getNodeCount(), initialNodeCount + 1);
     EXPECT_TRUE(transaction.isActive());
 
     transaction.rollback();
 
     EXPECT_FALSE(transaction.isActive());
-
     // Node should be removed after rollback
-    // Note: Would need additional MeshData methods to verify node removal
+    EXPECT_EQ(meshData_->getNodeCount(), initialNodeCount);
 }
 
 TEST_F(MeshTransactionTest, NodeModificationAndRollbackTest)
 {
+    // Get original coordinates
+    const Node* node = meshData_->getNode(node1Id_);
+    ASSERT_NE(node, nullptr);
+    auto originalCoords = node->getCoordinates();
+
     MeshTransaction transaction(meshData_.get());
     transaction.begin();
 
     // Modify an existing node during transaction
-    meshData_->moveNode(node1Id_, {1.5, 1.5, 1.5});
+    std::array<double, 3> newCoords = {1.5, 1.5, 1.5};
+    meshData_->moveNode(node1Id_, newCoords);
 
+    // Verify the node was modified
+    node = meshData_->getNode(node1Id_);
+    EXPECT_EQ(node->getCoordinates(), newCoords);
     EXPECT_TRUE(transaction.isActive());
 
     transaction.rollback();
 
     EXPECT_FALSE(transaction.isActive());
-
     // Node coordinates should be restored after rollback
-    // Note: Would need additional MeshData methods to verify coordinate restoration
+    node = meshData_->getNode(node1Id_);
+    EXPECT_EQ(node->getCoordinates(), originalCoords);
 }
 
 TEST_F(MeshTransactionTest, ElementRemovalAndRollbackTest)
@@ -170,24 +184,36 @@ TEST_F(MeshTransactionTest, ElementRemovalAndRollbackTest)
         std::array<size_t, 4>{node1Id_, node2Id_, node3Id_, node4Id_});
     size_t elementId = meshData_->addElement(std::move(element));
 
+    EXPECT_EQ(meshData_->getElementCount(), 1);
+    EXPECT_NE(meshData_->getElement(elementId), nullptr);
+
     // Now remove it within a transaction
     MeshTransaction transaction(meshData_.get());
     transaction.begin();
 
     meshData_->removeElement(elementId);
 
+    EXPECT_EQ(meshData_->getElementCount(), 0);
+    EXPECT_EQ(meshData_->getElement(elementId), nullptr);
     EXPECT_TRUE(transaction.isActive());
 
     transaction.rollback();
 
     EXPECT_FALSE(transaction.isActive());
-
     // Element should be restored after rollback
-    // Note: Would need additional MeshData methods to verify element restoration
+    EXPECT_EQ(meshData_->getElementCount(), 1);
+    EXPECT_NE(meshData_->getElement(elementId), nullptr);
 }
 
 TEST_F(MeshTransactionTest, MultipleOperationsRollbackTest)
 {
+    size_t initialNodeCount = meshData_->getNodeCount();
+    size_t initialElementCount = meshData_->getElementCount();
+
+    // Get original coordinates of node2
+    const Node* node2 = meshData_->getNode(node2Id_);
+    auto originalNode2Coords = node2->getCoordinates();
+
     MeshTransaction transaction(meshData_.get());
     transaction.begin();
 
@@ -199,18 +225,29 @@ TEST_F(MeshTransactionTest, MultipleOperationsRollbackTest)
         std::array<size_t, 4>{node1Id_, node3Id_, node4Id_, newNodeId});
     size_t elementId = meshData_->addElement(std::move(element));
 
+    // Verify operations were applied
+    EXPECT_EQ(meshData_->getNodeCount(), initialNodeCount + 1);
+    EXPECT_EQ(meshData_->getElementCount(), initialElementCount + 1);
+    node2 = meshData_->getNode(node2Id_);
+    EXPECT_NE(node2->getCoordinates(), originalNode2Coords);
     EXPECT_TRUE(transaction.isActive());
 
     transaction.rollback();
 
     EXPECT_FALSE(transaction.isActive());
-
     // All operations should be rolled back
-    // Note: Would need additional MeshData methods to verify complete rollback
+    EXPECT_EQ(meshData_->getNodeCount(), initialNodeCount);
+    EXPECT_EQ(meshData_->getElementCount(), initialElementCount);
+    node2 = meshData_->getNode(node2Id_);
+    EXPECT_EQ(node2->getCoordinates(), originalNode2Coords);
+    EXPECT_EQ(meshData_->getNode(newNodeId), nullptr);
+    EXPECT_EQ(meshData_->getElement(elementId), nullptr);
 }
 
 TEST_F(MeshTransactionTest, NestedTransactionBehaviorTest)
 {
+    EXPECT_EQ(meshData_->getElementCount(), 0);
+
     MeshTransaction transaction1(meshData_.get());
     transaction1.begin();
 
@@ -219,7 +256,9 @@ TEST_F(MeshTransactionTest, NestedTransactionBehaviorTest)
         std::array<size_t, 4>{node1Id_, node2Id_, node3Id_, node4Id_});
     size_t elementId1 = meshData_->addElement(std::move(element1));
 
+    EXPECT_EQ(meshData_->getElementCount(), 1);
     transaction1.commit();
+    EXPECT_EQ(meshData_->getElementCount(), 1);
 
     // Second transaction
     MeshTransaction transaction2(meshData_.get());
@@ -230,10 +269,13 @@ TEST_F(MeshTransactionTest, NestedTransactionBehaviorTest)
         std::array<size_t, 4>{node2Id_, node3Id_, node4Id_, node5Id_});
     size_t elementId2 = meshData_->addElement(std::move(element2));
 
+    EXPECT_EQ(meshData_->getElementCount(), 2);
     transaction2.rollback();
 
     // First element should remain, second should be rolled back
-    // Note: Would need additional MeshData methods to verify state
+    EXPECT_EQ(meshData_->getElementCount(), 1);
+    EXPECT_NE(meshData_->getElement(elementId1), nullptr);
+    EXPECT_EQ(meshData_->getElement(elementId2), nullptr);
 }
 
 // Tests for ScopedTransaction
@@ -262,6 +304,8 @@ protected:
 
 TEST_F(ScopedTransactionTest, AutoRollbackOnScopeExitTest)
 {
+    EXPECT_EQ(meshData_->getElementCount(), 0);
+
     {
         ScopedTransaction scopedTx(meshData_.get());
 
@@ -270,18 +314,19 @@ TEST_F(ScopedTransactionTest, AutoRollbackOnScopeExitTest)
             std::array<size_t, 4>{node1Id_, node2Id_, node3Id_, node4Id_});
         size_t elementId = meshData_->addElement(std::move(element));
 
-        // Transaction should be active
-        // Note: ScopedTransaction doesn't expose isActive(), but underlying transaction should be active
+        EXPECT_EQ(meshData_->getElementCount(), 1);
 
         // No explicit commit - should auto-rollback when scope ends
     }
 
     // Element should be removed after auto-rollback
-    // Note: Would need additional MeshData methods to verify element removal
+    EXPECT_EQ(meshData_->getElementCount(), 0);
 }
 
 TEST_F(ScopedTransactionTest, ExplicitCommitTest)
 {
+    EXPECT_EQ(meshData_->getElementCount(), 0);
+
     {
         ScopedTransaction scopedTx(meshData_.get());
 
@@ -289,6 +334,8 @@ TEST_F(ScopedTransactionTest, ExplicitCommitTest)
         auto element = std::make_unique<MockTetrahedralElement>(
             std::array<size_t, 4>{node1Id_, node2Id_, node3Id_, node4Id_});
         size_t elementId = meshData_->addElement(std::move(element));
+
+        EXPECT_EQ(meshData_->getElementCount(), 1);
 
         // Explicitly commit
         scopedTx.commit();
@@ -297,32 +344,51 @@ TEST_F(ScopedTransactionTest, ExplicitCommitTest)
     }
 
     // Element should remain after explicit commit
-    // Note: Would need additional MeshData methods to verify element persistence
+    EXPECT_EQ(meshData_->getElementCount(), 1);
 }
 
 TEST_F(ScopedTransactionTest, MultipleOperationsWithCommitTest)
 {
+    size_t initialNodeCount = meshData_->getNodeCount();
+    size_t initialElementCount = meshData_->getElementCount();
+
+    size_t newNodeId, elementId;
+
     {
         ScopedTransaction scopedTx(meshData_.get());
 
         // Perform multiple operations
-        size_t newNodeId = meshData_->addNode({2.0, 2.0, 2.0});
+        newNodeId = meshData_->addNode({2.0, 2.0, 2.0});
         meshData_->moveNode(node1Id_, {1.5, 1.5, 1.5});
 
         auto element = std::make_unique<MockTetrahedralElement>(
             std::array<size_t, 4>{node1Id_, node2Id_, node3Id_, newNodeId});
-        size_t elementId = meshData_->addElement(std::move(element));
+        elementId = meshData_->addElement(std::move(element));
+
+        EXPECT_EQ(meshData_->getNodeCount(), initialNodeCount + 1);
+        EXPECT_EQ(meshData_->getElementCount(), initialElementCount + 1);
 
         // Commit all operations
         scopedTx.commit();
     }
 
     // All operations should be preserved after commit
-    // Note: Would need additional MeshData methods to verify state
+    EXPECT_EQ(meshData_->getNodeCount(), initialNodeCount + 1);
+    EXPECT_EQ(meshData_->getElementCount(), initialElementCount + 1);
+    EXPECT_NE(meshData_->getNode(newNodeId), nullptr);
+    EXPECT_NE(meshData_->getElement(elementId), nullptr);
+
+    // Verify node coordinates were modified
+    const Node* node1 = meshData_->getNode(node1Id_);
+    std::array<double, 3> expectedCoords = {1.5, 1.5, 1.5};
+    EXPECT_EQ(node1->getCoordinates(), expectedCoords);
 }
 
 TEST_F(ScopedTransactionTest, PartialWorkRollbackTest)
 {
+    size_t initialNodeCount = meshData_->getNodeCount();
+    size_t initialElementCount = meshData_->getElementCount();
+
     {
         ScopedTransaction scopedTx(meshData_.get());
 
@@ -338,11 +404,16 @@ TEST_F(ScopedTransactionTest, PartialWorkRollbackTest)
             std::array<size_t, 4>{node1Id_, node3Id_, node4Id_, newNode2});
         size_t elementId2 = meshData_->addElement(std::move(element2));
 
+        // Verify operations were applied
+        EXPECT_EQ(meshData_->getNodeCount(), initialNodeCount + 2);
+        EXPECT_EQ(meshData_->getElementCount(), initialElementCount + 2);
+
         // Don't commit - let it auto-rollback
     }
 
     // All operations should be rolled back
-    // Note: Would need additional MeshData methods to verify complete rollback
+    EXPECT_EQ(meshData_->getNodeCount(), initialNodeCount);
+    EXPECT_EQ(meshData_->getElementCount(), initialElementCount);
 }
 
 } // namespace Operations
