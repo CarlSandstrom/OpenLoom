7474d12e74081509ec9a7169e5cb6a1d0afb5f49
Carl Sandström <sandstrom.carl@gmail.com>
2025-11-23 23:18:51 +0100

    Add BoxWithHole example

---
+++ src/Examples/BoxWithHole.cc
@@ -0,0 +1,36 @@
+#include "../Readers/OpenCascade/TopoDS_ShapeConverter.h"
+#include "Export/VtkExporter.h"
+#include "Meshing/Core/MeshingContext.h"
+#include "Meshing/Core/SimpleMesher.h"
+#include <BRepAlgoAPI_Cut.hxx>
+#include <BRepPrimAPI_MakeBox.hxx>
+#include <BRepPrimAPI_MakeCylinder.hxx>
+#include <TopoDS_Shape.hxx>
+#include <gp_Ax2.hxx>
+#include <gp_Dir.hxx>
+#include <gp_Pnt.hxx>
+
+int main()
+{
+    TopoDS_Shape box = BRepPrimAPI_MakeBox(10.0, 10.0, 10.0).Shape();
+
+    // Drill a cylindrical hole through the box center using a boolean cut.
+    gp_Pnt center(5.0, 5.0, 0.0);
+    gp_Dir axisDirection(0.0, 0.0, 1.0);
+    gp_Ax2 axis(center, axisDirection);
+    TopoDS_Shape cylinder = BRepPrimAPI_MakeCylinder(axis, 2.0, 10.0).Shape();
+    TopoDS_Shape boxWithHole = BRepAlgoAPI_Cut(box, cylinder).Shape();
+
+    Readers::TopoDS_ShapeConverter converter(boxWithHole);
+    Meshing::MeshingContext context(
+        converter.getGeometryCollection(),
+        converter.getTopology());
+
+    Meshing::SimpleMesher mesher;
+    mesher.generate(context);
+
+    Export::VtkExporter exporter;
+    exporter.writeVtu(context.getMeshData(), "BoxWithHole_mesh.vtu");
+
+    return 0;
+}
